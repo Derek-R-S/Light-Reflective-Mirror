0ed36a5a2f1030b34a46af38f0af8fcb9bf85b8c
Derek S <44935661+Derek-R-S@users.noreply.github.com>
2021-04-07 01:07:51 -0500

    Simple Web Transport

---
+++ ServerProject-DONT-IMPORT-INTO-UNITY/MultiCompiled/SimpleWebTransport/Common/Log.cs
@@ -0,0 +1,94 @@
+using System;
+using Conditional = System.Diagnostics.ConditionalAttribute;
+
+namespace Mirror.SimpleWeb
+{
+    public static class Log
+    {
+        // used for Conditional
+        const string SIMPLEWEB_LOG_ENABLED = nameof(SIMPLEWEB_LOG_ENABLED);
+        const string DEBUG = nameof(DEBUG);
+
+        public enum Levels
+        {
+            none = 0,
+            error = 1,
+            warn = 2,
+            info = 3,
+            verbose = 4,
+        }
+
+        public static Levels level = Levels.none;
+
+        public static string BufferToString(byte[] buffer, int offset = 0, int? length = null)
+        {
+            return BitConverter.ToString(buffer, offset, length ?? buffer.Length);
+        }
+
+        [Conditional(SIMPLEWEB_LOG_ENABLED)]
+        public static void DumpBuffer(string label, byte[] buffer, int offset, int length)
+        {
+            if (level < Levels.verbose)
+                return;
+
+        }
+
+        [Conditional(SIMPLEWEB_LOG_ENABLED)]
+        public static void DumpBuffer(string label, ArrayBuffer arrayBuffer)
+        {
+            if (level < Levels.verbose)
+                return;
+
+        }
+
+        [Conditional(SIMPLEWEB_LOG_ENABLED)]
+        public static void Verbose(string msg, bool showColor = true)
+        {
+            if (level < Levels.verbose)
+                return;
+        }
+
+        [Conditional(SIMPLEWEB_LOG_ENABLED)]
+        public static void Info(string msg, bool showColor = true)
+        {
+            if (level < Levels.info)
+                return;
+
+        }
+
+        /// <summary>
+        /// An expected Exception was caught, useful for debugging but not important
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="showColor"></param>
+        [Conditional(SIMPLEWEB_LOG_ENABLED)]
+        public static void InfoException(Exception e)
+        {
+            if (level < Levels.info)
+                return;
+
+        }
+
+        [Conditional(SIMPLEWEB_LOG_ENABLED), Conditional(DEBUG)]
+        public static void Warn(string msg, bool showColor = true)
+        {
+            if (level < Levels.warn)
+                return;
+
+        }
+
+        [Conditional(SIMPLEWEB_LOG_ENABLED), Conditional(DEBUG)]
+        public static void Error(string msg, bool showColor = true)
+        {
+            if (level < Levels.error)
+                return;
+
+        }
+
+        public static void Exception(Exception e)
+        {
+            // always log Exceptions
+            Console.WriteLine("SWT Exception: " + e);
+        }
+    }
+}
